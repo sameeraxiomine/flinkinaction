f7b7000084d852a4395594b9b35cce74f32034a1
SameerAxiomine <sameer@axiomine.com>
2016-06-26 09:21:06 -0400

    Update to seperate the lines to illustrate the return types.

--- flinkinactionjava/src/main/java/com/manning/fia/c04/SimpleStreamingExample.java
+++ flinkinactionjava/src/main/java/com/manning/fia/c04/SimpleStreamingExample.java
@@ -1,8 +1,13 @@
 package com.manning.fia.c04;
 
 import com.manning.fia.transformations.media.NewsFeedMapper;
+
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.shaded.com.google.common.base.Throwables;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
@@ -18,11 +23,11 @@ public class SimpleStreamingExample {
                     .createLocalEnvironment(1);
         DataStream<String> socketStream = execEnv.socketTextStream(
                     "localhost", 9000);
-        socketStream.map(new NewsFeedMapper())
-                    .project(1,2,4)
-                    .keyBy(0, 1)
-                    .sum(2)                    
-                    .print();
+        DataStream<Tuple3<String,String,Long>> selectDS = socketStream.map(new NewsFeedMapper())
+                    .project(1,2,4);
+        KeyedStream<Tuple3<String, String, Long>, Tuple> keyedDS = selectDS.keyBy(0, 1);
+        DataStream<Tuple3<String, String, Long>> result = keyedDS.sum(2);
+        result.print();
         execEnv.execute("Simple Streaming");
     }
 
